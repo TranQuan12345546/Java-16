a9b24f48444ab3c03221999884d153d04505951c
Quan <trananhquan335@gmail.com>
2022-10-22 17:32:21 +0700

    add bai2

---
+++ src/Baitapvenha01/bai2/ChuViHinhTron.java
@@ -0,0 +1,8 @@
+package Baitapvenha01.bai2;
+
+public class ChuViHinhTron {
+    public static void main(String[] args) {
+        double banKinh = 3;
+        System.out.println("Chu vi hình tròn là: " + Math.floor(2*Math.PI*banKinh));
+    }
+}
---
+++ src/Baitapvenha01/bai2/DienTichHinhTron.java
@@ -0,0 +1,8 @@
+package Baitapvenha01.bai2;
+
+public class DienTichHinhTron {
+    public static void main(String[] args) {
+        double banKinh = 3;
+        System.out.println("Diện tích hình tròn là: " + Math.floor(Math.PI*Math.pow(banKinh, 2)));
+    }
+}
