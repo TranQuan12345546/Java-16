34f93faa9790f9d04b3d1da0be441cc09ee26855
Quan <trananhquan335@gmail.com>
2022-11-01 18:19:06 +0700

    sua bai 11

--- src/BaiTapVeNha03/Bai11.java
+++ src/BaiTapVeNha03/Bai11.java
@@ -12,29 +12,25 @@ public class Bai11 {
         }
 
         int S = 0;
+        int S1 = 0;
+        int S2 = 0;
         for (int i = 1; i <= n ; i++) {
             S += i;
+            if(i % 2 != 0) {
+                S1 += i;
+            }
+            if(i % 2 == 0) {
+                S2 += i;
+            }
         }
         System.out.println("Tổng các số tự nhiên không lớn hơn " + n + " là " + S);
 
         System.out.println("--------------------------");
 
-        int S1 = 0;
-        for (int i = 1; i <= n ; i++) {
-            if(i % 2 != 0) {
-                S1 += i;
-            }
-        }
         System.out.println("Tổng các số tự nhiên lẻ không lớn hơn " + n + " là " + S1);
 
         System.out.println("--------------------------");
 
-        int S2 = 0;
-        for (int i = 1; i <= n ; i++) {
-            if(i % 2 == 0) {
-                S2 += i;
-            }
-        }
         System.out.println("Tổng các số tự nhiên chẵn không lớn hơn " + n + " là " + S2);
     }
     
